5dadd05cd101a2bd6cb3338b581ffc0f2de24607
Eduardo Pereira <pereira.somoza@gmail.com>
2012-05-01 15:25:59 -0300

    Adicionando arquivos, primeiro commit

--- converte.sh
+++ converte.sh
@@ -2,18 +2,34 @@
 # -*- coding: utf-8 -*-
 # Converte vob para avi
 
-if [ $1 ] && [ $2 ]; then
+if [ $1 ]; then
 
-echo "Os arquivos sao $1 e $2"
+    clear
+    
+    if [ "$1"=="-h" ]; then
+        echo " "
+        echo " Entre com o comando seguido dos nomes dos arquivos de "
+        echo " entrada (.vob) e saida (.avi)"
+        echo " ex:"
+        echo " "
+        echo "   ./converte.sh arquivo.vob arquivo.avi"
+        echo " "
+    fi
 
-ffmpeg -i $1 -f avi -vcodec mpeg4 -b 800k -g 300 -bf 2 $2
+    if [ $2 ]; then
+        echo "Os arquivos sao $1 e $2"
+
+        ffmpeg -i $1 -f avi -vcodec mpeg4 -b 800k -g 300 -bf 2 $2
+        
+    fi
 
 else
-echo 'Entre com o nome do arquivo de entrada (*.vob)'
-read ENTRADA
-echo 'Entre com o nome do arquivo de saida (*.avi)'
-read SAIDA
+    clear
+    echo 'Entre com o nome do arquivo de entrada (*.vob)'
+    read ENTRADA
+    echo 'Entre com o nome do arquivo de saida (*.avi)'
+    read SAIDA
 
-ffmpeg -i $ENTRADA -f avi -vcodec mpeg4 -b 800k -g 300 -bf 2 $SAIDA
+    ffmpeg -i $ENTRADA -f avi -vcodec mpeg4 -b 800k -g 300 -bf 2 $SAIDA
 
 fi
